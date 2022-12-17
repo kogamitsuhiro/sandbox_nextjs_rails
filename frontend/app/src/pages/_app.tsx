554491158cf8a12821eb5936f4d752b5bcc7c954
kogamitsuhiro <utirn.ame@gmail.com>
2022-12-17 22:36:00 +0900

    auth0

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const { user } = pageProps;
+
+  return (
+    <UserProvider user={user}>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp;
