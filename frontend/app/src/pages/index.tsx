19e93dc614db061382b732d867b51907c1d75f52
kogamitsuhiro <utirn.ame@gmail.com>
2022-12-19 08:11:35 +0900

    fix

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
@@ -9,10 +10,10 @@ export default function Index() {
   if (user) {
     return (
       <div>
-        Welcome {user.name}! <a href='/api/auth/logout'>Logout</a>
+        Welcome {user.name}! <Link href='/api/auth/logout'>Logout</Link>
       </div>
     );
   }
 
-  return <a href='/api/auth/login'>Login</a>;
+  return <Link href='/api/auth/login'>Login</Link>;
 }
