5380aaf245493b387f8ff7c91ce7d065c7d4d972
kogamitsuhiro <utirn.ame@gmail.com>
2022-12-28 06:46:27 +0900

    add 404

---
+++ frontend/app/src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layouts/basic';
+
+export default function Index() {
+  return (
+    <Layout>
+      <div>
+        <p>Not Found</p>
+      </div>
+    </Layout>
+  );
+}
